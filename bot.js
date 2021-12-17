875928bfd3ecfc3be2f57d40443c93022368ebdd
cnscdh <84962597+cnscdh@users.noreply.github.com>
2021-12-17 16:19:44 +0800

    修复：出现错误时，机器人会尝试恢复工作

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-//农民世界机器人V1.3
+//农民世界机器人V1.5
 (async () => {
   // 可选择要执行的的地图
   // 1 — 矿业
@@ -8,95 +8,145 @@
   // 例如：const availableMaps = [1];表示执行矿业
   const availableMaps = [1, 2, 3, 4];
   // 移动到下一个地图之间的延迟（5秒）
-  const delayNextMap = 5 * 1000;
+  const delayNextMap = [5 * 1000, 15 * 1000];
   // 地图选择后的延迟（5秒）
-  const delayAfterMapSelect = 5 * 1000;
+  const delayAfterMapSelect = [5 * 1000, 15 * 1000];
   // 找到后的延迟（1秒）
-  const delayAfterMine = 1 * 1000;
+  const delayAfterMine = [10 * 1000, 25 * 1000];
   // 维修开始前的延迟（10秒）
-  const delayBeforeRepair = 10 * 1000;
+  const delayBeforeRepair = [8 * 1000, 15 * 1000];
   // 维修开始后延迟（1秒）
-  const delayAfterRepair = 1 * 1000;
+  const delayAfterRepair = [1 * 1000, 3 * 1000];
+
+  const random = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
+  setInterval(() => {
+    const buttonClosePopup = document.querySelector(
+      ".modal .plain-button.short"
+    );
+
+    if (buttonClosePopup) buttonClosePopup.click();
+  }, random(1, 2) * 1000);
+
+  setInterval(() => {
+    const buttonCloseCPUPopup = document.querySelector(
+      ".modal-stake .modal-stake-close img"
+    );
+
+    if (buttonCloseCPUPopup) buttonCloseCPUPopup.click();
+  }, random(1, 2) * 1000);
 
   const mapBtn = document.querySelector(".navbar-group--icon[alt='Map']");
   mapBtn.click();
 
   while (1) {
-    for (let mapId = 0; mapId < 4; ++mapId) {
-      if (!availableMaps.includes(mapId + 1)) continue;
+    try {
+      for (let mapId = 0; mapId < 4; ++mapId) {
+        if (!availableMaps.includes(mapId + 1)) continue;
 
-      await new Promise((res) => setTimeout(res, delayNextMap));
+        await new Promise((res) => setTimeout(res, random(...delayNextMap)));
 
-      const map = document.querySelectorAll(".map-container-bg")[mapId];
+        const map = document.querySelectorAll(".map-container-bg")[mapId];
 
-      if (map.style.filter === "grayscale(1)") continue;
+        if (map.style.filter === "grayscale(1)") continue;
 
-      map.click();
+        map.click();
 
-      await new Promise((res) => setTimeout(res, delayAfterMapSelect));
-
-      for (const [indexItem, item] of document
-        .querySelectorAll(".vertical-carousel-container img")
-        .entries()) {
-        item.click();
-
-        await new Promise((res) => setTimeout(res, 1e3));
-
-        const buttonMine = document.querySelector(
-          ".info-section .plain-button"
+        await new Promise((res) =>
+          setTimeout(res, random(...delayAfterMapSelect))
         );
-        const timeToEnd = document.querySelector(
-          ".info-section .info-time"
-        ).innerText;
-        if (
-          ![...buttonMine.classList].includes("disabled") &&
-          timeToEnd === "00:00:00"
-        ) {
-          buttonMine.click();
 
-          await new Promise((res) => setTimeout(res, delayAfterMine));
+        for (const [, item] of document
+          .querySelectorAll(".vertical-carousel-container img")
+          .entries()) {
+          // 恢复能量开始
+          const currentFish = Math.floor(
+            +document.querySelectorAll(".resource-number")[2].innerText
+          );
+          const [currentEnergy, maxEnergy] = document
+            .querySelectorAll(".resource-number")[3]
+            .textContent.split("/")
+            .map(Number);
 
-          // 选择地图并检查
-          if (mapId === 0) {
-            await new Promise((res) => setTimeout(res, delayBeforeRepair));
-
-            // 维修工具
-            const buttonRepair = document.querySelectorAll(
-              ".info-section .plain-button"
-            )[1];
-            const quality = eval(
-              document.querySelector(".card-number").innerText
+          if (maxEnergy - currentEnergy > 100 && currentFish > 1) {
+            const countEnergyClicks = Math.min(
+              currentFish,
+              Math.floor((maxEnergy - currentEnergy) / 5)
             );
-            if (
-              ![...buttonRepair.classList].includes("disabled") &&
-              quality < 0.5
-            ) {
-              buttonRepair.click();
-              await new Promise((res) => setTimeout(res, delayAfterRepair));
+
+            if (countEnergyClicks > 0) {
+              document.querySelector(".resource-energy img").click();
+              await new Promise((res) => setTimeout(res, random(1, 2) * 1000));
+
+              for (let i = 0; i++ < countEnergyClicks; ) {
+                document
+                  .querySelector(".image-button[alt='Plus Icon']")
+                  .click();
+                await new Promise((res) =>
+                  setTimeout(res, random(2, 10) * 100)
+                );
+              }
+              document.querySelector(".modal-wrapper .plain-button").click();
+              await new Promise((res) =>
+                setTimeout(res, random(15, 15) * 1000)
+              );
             }
           }
-            // 补充能量（小于200能量添加100能量）可调整数值
-          const currentEnergy = +document.querySelectorAll(
-            ".resource-number div"
-          )[3].innerText;
-          const currentFish =
-            +document.querySelectorAll(".resource-number")[2].innerText;
-          if (currentEnergy < 200 && currentFish > 20) {
-            document.querySelector(".resource-energy img").click();
-            await new Promise((res) => setTimeout(res, 1e3));
+          // 恢复能量结束
 
-            for (let i = 0; i++ < 20; ) {
-              document.querySelector(".image-button[alt='Plus Icon']").click();
-              await new Promise((res) => setTimeout(res, 5e2));
+          item.click();
+
+          await new Promise((res) => setTimeout(res, random(1, 2) * 1000));
+
+          const buttonMine = document.querySelector(
+            ".info-section .plain-button"
+          );
+          const timeToEnd = document.querySelector(
+            ".info-section .info-time"
+          ).innerText;
+
+          if (
+            ![...buttonMine.classList].includes("disabled") &&
+            (timeToEnd === "00:00:00" || mapId === 0)
+          ) {
+            buttonMine.click();
+
+            await new Promise((res) =>
+              setTimeout(res, random(...delayAfterMine))
+            );
+
+            // 判断地图和执行
+            if (mapId === 0) {
+              await new Promise((res) =>
+                setTimeout(res, random(...delayBeforeRepair))
+              );
+
+              // 维修工具
+              const buttonRepair = document.querySelectorAll(
+                ".info-section .plain-button"
+              )[1];
+              if (buttonRepair) {
+                const quality = eval(
+                  document.querySelector(".card-number").innerText
+                );
+                if (
+                  ![...buttonRepair.classList].includes("disabled") &&
+                  quality < 0.5
+                ) {
+                  buttonRepair.click();
+                  await new Promise((res) =>
+                    setTimeout(res, random(...delayAfterRepair))
+                  );
+                }
+              }
             }
-
-            document.querySelector(".modal-wrapper .plain-button").click();
-
-            await new Promise((res) => setTimeout(res, 2e4));
           }
         }
-      }
 
+        mapBtn.click();
+      }
+    } catch (e) {
       mapBtn.click();
     }
   }
